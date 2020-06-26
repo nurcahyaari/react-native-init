8789c5d57496905a0ee9fd8e412d51dc088e22a5
Ari Nurcahya <nurcahyaari@gmail.com>
2020-06-26 09:47:20 +0700

    change app name

--- android/app/src/main/java/com/reactnativeinit/MainActivity.java
+++ android/app/src/main/java/com/reactnativeinit/MainActivity.java
@@ -1,4 +1,4 @@
-package com.mobile_keuangan;
+package com.reactnativeinit;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "mobile_keuangan";
+    return "reactnativeinit";
   }
 }
--- ios/reactnativeinitTests/reactnativeinitTests.m
+++ ios/reactnativeinitTests/reactnativeinitTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface mobile_keuanganTests : XCTestCase
+@interface reactnativeinitTests : XCTestCase
 
 @end
 
-@implementation mobile_keuanganTests
+@implementation reactnativeinitTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
